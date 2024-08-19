05a9db2a30ae82c14adba9bc823b69d589be3f36
Bautista <bautiverna@gmail.com>
2024-08-19 19:14:50 -0300

    Agrgando la funcion de mostrar el inventario

---
+++ src/main/java/edu/uca/Ropa.java
@@ -0,0 +1,9 @@
+package edu.uca;
+
+public class Ropa {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    public Ropa(String nombre, String apellido, int edad) {}
+}
+
